1307be612b96874752b2ed6eb5d1d1f04c7ff1f9
Lavinya Correia <lavinya.correia01@gmail.com>
2021-08-04 15:09:22 -0300

    Possibilitar a mudança de ascendente para descendente - ORDENAÇÃO

--- src/app/componentes/inicial/inicial.component.ts
+++ src/app/componentes/inicial/inicial.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarefa } from 'src/app/models/tarefa.model';
 import { TarefaService } from 'src/app/servicos/tarefa.service';
+import { Ordenacao } from 'src/app/utils/ordenacao.enum';
 
 @Component({
   selector: 'app-inicial',
@@ -11,24 +12,38 @@ export class InicialComponent implements OnInit {
 
   tarefas: Tarefa[]=[];
   tarefa: Tarefa = new Tarefa('','',false);
+  ordem: Ordenacao = Ordenacao.ASC;
   
   constructor(private tarefaService: TarefaService) { }
 
   ngOnInit(): void {
-    this.tarefas = this.tarefaService.listarTodos();
+    this.tarefas = this.tarefaService.listarTodos(this.ordem);
   }
 
   completeTask(id: string) {
     this.tarefaService.completeTask(id);
-    this.tarefas = this.tarefaService.listarTodos();
+    this.tarefas = this.tarefaService.listarTodos(this.ordem);
   }
 
   removerId(tarefaId: string){
-      this.tarefa = this.tarefaService.listarId(tarefaId);
+    this.tarefas = this.tarefaService.listarTodos(this.ordem);
   }
 
   remover() {
     this.tarefaService.remover(this.tarefa.id);
-    this.tarefas = this.tarefaService.listarTodos();
+    this.tarefas = this.tarefaService.listarTodos(this.ordem);
+  }
+
+  ordenar() {
+    if (this.ordem === Ordenacao.ASC) {
+      this.ordem = Ordenacao.DESC;
+    } else {
+      this.ordem = Ordenacao.ASC;
+    }
+    this.tarefas = this.tarefaService.listarTodos(this.ordem);
+  }
+
+  ascendente() {
+    return this.ordem === Ordenacao.ASC;
   }
 }
